c962111efec2c3fdeae980fc9f3322c0592cf69d
Maria Campo <mariacampo@Marias-Air.lan>
2025-05-15 07:45:02 -0700

    Small changes

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# road-patrol
+# breakchekr
+
+To run frontend:
+- cd client  
+- npm run dev
+
+To set up local db:
+- docker-compose up -d  
+
+To halt local db: 
+- docker-compose down  
--- server/config/config.js
+++ server/config/config.js
@@ -1,23 +1,11 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+require('dotenv').config({ path: '../.env' }); // loads variables from .env
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER || 'devuser',
+    password: process.env.DB_PASS || 'devpass',
+    database: process.env.DB_NAME || 'devdb',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres'
   }
-}
+};
